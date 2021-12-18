5b7a9a9bb348e0eb1492b8548742b10a105380fb
tea.zup <tea.zupancic3@gmail.com>
2021-12-18 19:02:26 +0100

    Komentar zbrisan

--- api/prevozi.php
+++ api/prevozi.php
@@ -37,21 +37,6 @@ function vsiPrevozi(){
     $odgovor[] = $vrstica;
   }
 
-  // echo "<div class='container-fluid' style='margin-top: 50px;'>";
-  // echo "<div class='row' style='background-color: white'>";
-  // echo "<table class='table table-striped'>";
-  // echo "<thead><tr><th>Kraj odhoda</th><th>Kraj prihoda</th><th>Čas odhoda</th><th>Cena</th></tr></thead><tbody>";
-  //
-  // $poizvedba = "SELECT * FROM prevozi WHERE cas_odhoda > NOW() ORDER BY kraj_odhoda DESC, kraj_prihoda DESC, cas_odhoda ASC";
-  // $rezultat = mysqli_query($zbirka, $poizvedba);
-  // while($vrstica = mysqli_fetch_assoc($rezultat)){
-  //   $kraj_odhoda = $vrstica["kraj_odhoda"];
-  //   $kraj_prihoda = $vrstica["kraj_prihoda"];
-  //   $cas_odhoda = date('d-m-Y H:i', strtotime($vrstica["cas_odhoda"]));
-  //   $cena = $vrstica["cena"];
-  //   echo "<tr><td>$kraj_odhoda</td><td>$kraj_prihoda</td><td>$cas_odhoda</td><td>$cena €</td></tr>";
-  // }
-  // echo "</tbody></table></div></div>";
   http_response_code(200);
   echo json_encode($odgovor);
 }
