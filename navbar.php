d7d85913a205d874c0d9d69401b1bc4ce3e4848c
tea.zup <tea.zupancic3@gmail.com>
2021-12-27 23:37:53 +0100

    Dodano preverjanje ce je uporabnik prijavljen

--- navbar.php
+++ navbar.php
@@ -26,3 +26,10 @@
     </ul>
 </div>
 </nav>
+
+<?php
+session_start();
+if (!isset($_SESSION["loggedin"])){ #na vsaki strani (razen prijavi in registraciji) preverimo, če je uporabnik prijavljen
+  header("Location: prijava.php"); #ce uporabnik ni prijavljen ga preusmerimo na prijavo
+}
+?>
